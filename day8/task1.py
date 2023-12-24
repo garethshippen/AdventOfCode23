15de3bf43957fe7e5fb5ca3255a0fec8ee035c57
Gareth Shippen <garethshippen@yahoo.co.uk>
2023-12-24 18:54:37 +0000

    Day 8 task 1 complete

--- day8/task1.py
+++ day8/task1.py
@@ -1,4 +1,5 @@
-inp = "21 32 43 50 52 63 128 354 980 2536 6198 14571 33400 75257 167315 367301 795349 1696172 3558635 7344376 14925482"
+#inp = "21 32 43 50 52 63 128 354 980 2536 6198 14571 33400 75257 167315 367301 795349 1696172 3558635 7344376 14925482"
+#inp = "0 3 6 9 12 15"
 
 def conv_to_ints(sequence):
     nums = sequence.split(" ")
@@ -10,17 +11,49 @@ def conv_to_ints(sequence):
 def calc_gaps(sequence):
     gaps = []
     for i in range(len(sequence)-1):
-        #print(sequence[i], sequence[i + 1], sequence[i+1] - sequence[i])
         gaps.append(sequence[i + 1] - sequence[i])
     return gaps
+
+def check_not_zeros(sequence): # Returns true if there is a non-zero in the input
+    for num in sequence:
+        if num != 0:
+            return True
+    return False
 
 def levels(sequence):
     if isinstance(sequence[0], str):
         sequence = conv_to_ints(sequence)
     rows = []
-    while(sum(calc_gaps(sequence))) != 0:
-        rows.append(calc_gaps(sequence))
+    rows.append(sequence)
+    while(check_not_zeros(calc_gaps(sequence))):
+        rows.insert(0,calc_gaps(sequence))
         sequence = calc_gaps(sequence)
-    rows.append([0 for i in range(len(rows[-1]) - 1)])
+    rows.insert(0,[0 for i in range(len(rows[0]) - 1)])
     return rows
 
+def predict(rows):
+    for i, row in enumerate(rows):
+        if i == 0:
+            rows[0].append(0)
+        else:
+            row.append(row[-1] + rows[i-1][-1])
+    return rows
+
+def pipeline(sequence):
+    l = levels(sequence)
+    return predict(l)[-1][-1]
+
+filename = "day8\\input.txt"
+#filename = "day8\\testinput.txt"
+
+inp = []
+with open(filename) as source:
+    for line in source.readlines():
+        inp.append(line.replace("\n", ""))
+
+total = 0
+for seq in inp:
+    total += pipeline(seq)
+
+print(total)
+# too high

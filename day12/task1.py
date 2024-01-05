411ee17a3d6c035671eb8c31c151e2f13c4d95b9
Gareth Shippen <garethshippen@yahoo.co.uk>
2024-01-05 15:18:57 +0000

    Day 12 task 1 complete

--- day12/task1.py
+++ day12/task1.py
@@ -1,4 +1,5 @@
 filename = "day12\\input.txt"
+#filename = "day12\\testinput.txt"
 
 raw = []
 with open(filename) as source:
@@ -16,3 +17,34 @@ for line in raw:
         pattern = []
 patterns.append(pattern)
 
+def transpose(matrix):
+    return ["".join(i) for i in list(map(list,zip(*matrix)))]
+
+def is_mirror(matrix, index):
+    size = len(matrix)
+    if index >= size - 1:
+        return False
+    
+    a = index
+    b = index + 1
+    while matrix[a] == matrix[b]:
+        a -= 1
+        b += 1
+        if a < 0 or b >= size:
+            return True
+    return False
+
+def mirror_index(matrix):
+    for i in range(len(matrix)):
+        if is_mirror(matrix, i):
+            return (i + 1) * 100
+    matrix = transpose(matrix)
+    for i in range(len(matrix)):
+        if is_mirror(matrix, i):
+            return i + 1
+
+total = 0
+for mat in patterns:
+    total += mirror_index(mat)
+print(total)
+#36041

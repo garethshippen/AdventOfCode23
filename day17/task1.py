6f96c50c3375516e65d1d785e5bfb208914b73e8
Gareth Shippen <garethshippen@yahoo.co.uk>
2024-02-17 16:01:44 +0000

    Task 1 complete

---
+++ day17/task1.py
@@ -0,0 +1,47 @@
+import cmath
+
+filename = "day17/input.txt"
+#filename = "day17/testinput.txt"
+
+def determ(point_a, point_b):
+    return point_a[0] * point_b[1] - point_b[0] * point_a[1] 
+
+def shoelace(points):
+    total = 0
+    for i in range(len(points)-1):
+        a = points[i]
+        b = points[i+1]
+        total += determ(a,b)
+    return total/2
+
+
+def setup(filename):
+    raw = []
+    with open(filename) as source:
+        for line in source.readlines():
+            raw.append(line)
+    return raw
+raw = setup(filename)
+
+def parse(lines):
+    directions = []
+    for line in lines:
+        bits = line.split()
+        directions.append((bits[0].lower(), int(bits[1])))
+    return directions
+instructions = parse(raw)
+
+directions = {'u': complex(0,-1), 'r': complex(1,0), 'd': complex(0,1), 'l': complex(-1,0)}
+
+coords = [(0,0)]
+
+loc = complex(0,0)
+perimeter = 0
+for instruction in instructions:
+    direction = directions[instruction[0]]
+    loc += (direction * instruction[1])
+    coords.append((int(loc.real), int(loc.imag)))
+    perimeter += instruction[1]
+
+print(shoelace(coords) + perimeter/2 + 1)
+# 35244
